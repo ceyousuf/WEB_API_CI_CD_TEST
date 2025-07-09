f4ed4514ddda26e4089c3ed6101605812389135e
Muhammad Abu Yousof <ce.yousuf@gmail.com>
2025-07-09 11:31:08 +0600

    UserName controller add

---
+++ WEB_API_CI_CD_TEST/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WEB_API_CI_CD_TEST.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpPost("/getUserName")]
+        public String UserName()
+        {
+
+
+           
+
+            return "Muhammad Abu Yousof";
+
+        }
+    }
+}
